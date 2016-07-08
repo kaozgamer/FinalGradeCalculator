89ac13666c55c8830adabd7af3e4a09fcda2ce53
kaozgamer <ktdp11195@yahoo.com>
2016-07-08 17:28:09 +0800

    Added option to contact developer in the settings.

--- app/src/main/java/com/thunderboltsoft/finalgradecalculator/fragments/SettingFragment.java
+++ app/src/main/java/com/thunderboltsoft/finalgradecalculator/fragments/SettingFragment.java
@@ -1,6 +1,8 @@
 package com.thunderboltsoft.finalgradecalculator.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.thunderboltsoft.finalgradecalculator.R;
@@ -13,5 +15,20 @@ public class SettingFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.fragment_setting);
+        Preference contactDeveloper = findPreference("pref_contact_dev");
+
+        contactDeveloper.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent mailto = new Intent(Intent.ACTION_SEND);
+                mailto.setType("message/rfc822");
+                mailto.putExtra(Intent.EXTRA_EMAIL, new String[]{"kaozgamerdev+grade_calculator@gmail.com"});
+                mailto.putExtra(Intent.EXTRA_SUBJECT, "Feedback about Grade Calculator");
+                mailto.putExtra(Intent.EXTRA_TEXT, "What can I help you with?");
+                startActivity(Intent.createChooser(mailto, "Select email application"));
+
+                return true;
+            }
+        });
     }
 }
