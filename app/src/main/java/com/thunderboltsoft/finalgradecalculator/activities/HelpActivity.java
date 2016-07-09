883636af4c65976ce85d6677c1282f641f85dc11
kaozgamer <ktdp11195@yahoo.com>
2016-07-09 14:24:23 +0800

    Added send email intents to the bug report and give feedback icons in the help screen.

--- app/src/main/java/com/thunderboltsoft/finalgradecalculator/activities/HelpActivity.java
+++ app/src/main/java/com/thunderboltsoft/finalgradecalculator/activities/HelpActivity.java
@@ -1,8 +1,10 @@
 package com.thunderboltsoft.finalgradecalculator.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -34,9 +36,36 @@ public class HelpActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_help, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         if (item.getItemId() == android.R.id.home) {
             finish();
+
+            return true;
+        } else if (item.getItemId() == R.id.action_help_report_problem) {
+            Intent mailto = new Intent(Intent.ACTION_SEND);
+            mailto.setType("message/rfc822");
+            mailto.putExtra(Intent.EXTRA_EMAIL, new String[]{"kaozgamerdev+grade_calculator@gmail.com"});
+            mailto.putExtra(Intent.EXTRA_SUBJECT, "Bug Report: Grade Calculator");
+            mailto.putExtra(Intent.EXTRA_TEXT, "What can I help you with?\n");
+            startActivity(Intent.createChooser(mailto, "Select email application"));
+
+            return true;
+        } else if (item.getItemId() == R.id.action_help_give_feedback) {
+            Intent mailto = new Intent(Intent.ACTION_SEND);
+            mailto.setType("message/rfc822");
+            mailto.putExtra(Intent.EXTRA_EMAIL, new String[]{"kaozgamerdev+grade_calculator@gmail.com"});
+            mailto.putExtra(Intent.EXTRA_SUBJECT, "Feedback: Grade Calculator");
+            mailto.putExtra(Intent.EXTRA_TEXT, "What can I help you with?\n");
+            startActivity(Intent.createChooser(mailto, "Select email application"));
+
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
